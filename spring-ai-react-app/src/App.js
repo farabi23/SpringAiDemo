aab3eadcc1ba6eb804fe54bf51aab7b0d86d8238
farabi23 <78350272+farabi23@users.noreply.github.com>
2025-01-24 01:33:33 +0500

    create recipe api front-end & ask ai

--- spring-ai-react-app/src/App.js
+++ spring-ai-react-app/src/App.js
@@ -21,7 +21,7 @@ function App() {
       onClick={() => handleTabChange('image-generator')}>Image Generator</button>
 
       <button className={activeTab === 'chat' ? 'active': ''} 
-       onClick={() => handleTabChange('chat')}>Chat</button>
+       onClick={() => handleTabChange('chat')}>Ask AI</button>
 
       <button className={activeTab === 'recipe-generator' ? 'active': ''}  
       onClick={() => handleTabChange('recipe-generator')}>Recipe Generator</button>
--- spring-ai-react-app/src/components/RecipeGenerator.js
+++ spring-ai-react-app/src/components/RecipeGenerator.js
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 function RecipeGenerator(){
+    const [ingredients, setIngredients] = useState('');
+    const [cuisine, setCuisine] = useState('any');
+    const [dietaryResctrictions, setDietaryResctrictions] = useState('');
+    const [recipe, setRecipe] = useState('');
+
+
+    const createRecipe = async() =>{
+
+        try{
+            const response = await fetch(`http://localhost:8080/recipe-creator?ingredients=${ingredients}&cuisine=${cuisine}&dietaryRestrictions=${dietaryResctrictions}`);
+            const data = await response.text();
+
+            console.log(data);
+
+            setRecipe(data);
+        }
+        catch (error){
+            console.error("Error creating recipe : ", error);
+            
+        }
+
+    }
+
     return (
-        <h2>Generate Recipe</h2>
+        <div>
+            <h2>Generate Recipe</h2>
+            <input type="text" value={ingredients} onChange={(e) => setIngredients(e.target.value)}
+            placeholder="Enter ingredients (comma separated)"/>
+
+            <input type="text" value={cuisine} onChange={(e) => setCuisine(e.target.value)}
+            placeholder="Enter cuisine"/>
+
+            <input type="text" value={dietaryResctrictions} onChange={(e) => setDietaryResctrictions(e.target.value)}
+            placeholder="Enter dietary restrictions"/>
+
+            <button onClick={createRecipe}>Create recipe</button>
+
+            <div className="output">
+                <pre className="recipe-text">
+                    {recipe}
+                </pre>
+            </div>
+
+        </div>
+        
     );
 }
 
